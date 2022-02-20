e1d3a3c3e2661e6a8de2635b8f0994e3dd468aae
Williams Ohworuka <williams@automedsys.com>
2022-02-20 18:12:11 +0100

    Deploy Next.js to gh-pages

--- _next/static/gZaen5SjYpE9Z2zhTBQZl/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/ea88be26-9c1bc965bb5248dc.js","static/css/6938ca2305d975bc.css","static/chunks/pages/index-772bc06b08960501.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
