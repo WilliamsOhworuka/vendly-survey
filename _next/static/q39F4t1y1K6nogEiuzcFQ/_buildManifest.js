5d75453bfffbb8f6f0ce4e9e1e82a855b2761c28
Williams Ohworuka <williams@automedsys.com>
2022-02-20 18:15:21 +0100

    Deploy Next.js to gh-pages

--- _next/static/q39F4t1y1K6nogEiuzcFQ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/ea88be26-9c1bc965bb5248dc.js","static/css/6938ca2305d975bc.css","static/chunks/pages/index-9a13968a58b162dd.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/tNiHXnvFRlAVJdGTzLqmg/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/ea88be26-9c1bc965bb5248dc.js","static/css/6938ca2305d975bc.css","static/chunks/pages/index-56532a3bd09761ce.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
